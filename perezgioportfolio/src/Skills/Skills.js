26a6f97cb886508106fe4815264204045957a2f6
Giovanni Perez <giovanniperez@Giovannis-MacBook-Pro.local>
2018-02-20 11:34:42 -0600

    Added skills

--- perezgioportfolio/src/Skills/Skills.js
+++ perezgioportfolio/src/Skills/Skills.js
@@ -14,11 +14,49 @@ class Skills extends Component{
 				<div id="skills">
 
 
-					<h1>Look at my skills!!!!</h1>
+					<div class="container">
+						<div class="row">
+							<ul class="col-2"></ul>
+							<ul class="col-6">
+								<h3>Primary Skills</h3>
+								<br/>
+								<li>Javascript/CSS/HTML</li>
+								<li>Bootstrap</li>
+								<li>SQL</li>
+								<li>Ruby</li>
+								<li>React</li>
+								<li>Sinatra</li>
+								<li>Node.js</li>
+								<li>MongoDB</li>
+								<li>Docker</li>
+								<li>Python</li>
+								<li>Hyperledger</li>
+								<li>Github</li>
+								<li>Salesforce</li>
+								<li>Gulp</li>
+								<li>Mocha/Chai</li>
+								
+							</ul>
+
+							<ul class="col-4">
+								<h3>Exposed to</h3>
+								<br/>
+
+								<li>Tableau</li>
+								<li>D3</li>
+								<li>Postman/JSON</li>
+								<li>JIRA</li>
+								<li>Agile</li>
+							</ul>
+						</div>
+					</div>
 					
+				
+
+
+
+
 				</div>
-
-
 			</div>
 
 			)
