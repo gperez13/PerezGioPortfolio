1f2137bbf998b661ec9146e50d451fb50f4b31c3
Giovanni Perez <giovanniperez@Giovannis-MacBook-Pro.local>
2018-02-20 11:16:41 -0600

    Changed SPA styling, added font links, updated project contents. Still need to add images and call on them from css/js components

--- perezgioportfolio/src/About/About.js
+++ perezgioportfolio/src/About/About.js
@@ -12,9 +12,34 @@ class About extends Component{
 		return(
 			<div>
 				<div id="about">
+						  <h3>From Chicago to Austin and Back</h3>
+	
+
+						  <br/><br/>
+					 I am a software developer with a knack for fostering 
+					 relationships between people and data. Two years ago I 
+					 moved to Austin, Texas and began working in the tech 
+					 industry. The Advisory Board Company introduced me to 
+					 what it's like to work in an enterprise setting and 
+					 gave me a behind the scenes understanding of both 
+					 software product as well as healthcare operations. 
+					 Now that I am back in Chicago I am looking to contribute 
+					 the performance of one of the best cities in the world. 
+					 Because of my background in informatics, I can focus on 
+					 immediate needs while keeping track of the big picture. 
+						<br/><br/><br/>
 
 
-					<h1>Here is an About page!!!</h1>
+
+
+					Creating community has always come easy to me. 
+					As an extrovert, I look to break down walls and 
+					introduce people to each other. Operating with a 
+					positive-sum mentality, I build up my peers and share 
+					opportunities for the benefit of the organization as 
+					a whole. Through these soft skills I can simplify 
+					complex ideas and deliver them to the end user as 
+					readable data. 
 					
 				</div>
 
--- perezgioportfolio/src/Projects/Projects.js
+++ perezgioportfolio/src/Projects/Projects.js
@@ -10,18 +10,122 @@ class Projects extends Component{
 
 
 		return(
+			
 			<div>
 				<div id="projects">
 
 
-					<h1>Check this out!!! Git Included</h1>
+
+					<h2>Work</h2>
+					<hr/><br/><br/><br/>
+
+
+					<div class="container">
+
+
+
+
+					<div class="row">
+
+						<div class="col-md-7">
+							<a href="https://rate-your-chicago-aldermen.herokuapp.com/" rel="noopener noreferrer" target="_blank"><img class="work" src="images/RateMyChicago.png"/></a>
+							
+						</div>
+							<div class="col-md-5">
+							
+							<h4>Rate My Chicago Alderman</h4>
+							<br/>
+
+								<p>Rate My Chicago Alderman is a politician rating project aimed at engaging Chicago citizens in 
+								local politics. It has been deployed with Heroku, but I am thinking about redeploying it on a 
+								larger server in case it picks up traffic. Prior to redeployment, we will fix some routing issues 
+								that render the wards incorrectly and work with the Sessions so that users can only alter records they create. 
+
+								<br/><br/>
+								Built Using: Javascript. D3.js. HTML5. CSS3. JSON. MongoDB. mLabm. Sessions. Express.
+								</p>
+
+
+							<a href="https://github.com/gperez13/my-CRUD-politician" rel="noopener noreferrer" target="_blank"><img id="kittycat" src="images/GitHubKitty.png" /></a>
+							</div>
+							
 					
+
+						<br/><br/><br/><hr/><br/><br/><br/>
+
+
+
+						<div class="row">
+
+							<div class="col-md-5">
+									<h4>BulbasaurGuac</h4>
+									<br/>
+
+									<p>This is a video game resembling one of many space shooter games from the 80's.  
+									Using Phaser libraries, I was able to add a physics element to manipulate objects in a canvas. 
+									The project allowed me to familiarize myself with Tiled,  jQuery, and DOM manipulation. In the 
+									future I would like to clean up the code thoroughly  as well as add animation onto the sprites. 
+									A bug also occurs when winning and losing simultaneously. The game can also benefit from additional 
+									features such as power ups or making enemies less predictable.
+
+
+									<br/><br/>
+									Built Using: Phaser. jQuery. HTML5. Tiled.
+									</p>
+									<a href="https://github.com/gperez13/BulbasaurGuac" rel="noopener noreferrer" target="_blank"><img id="kittycat" src="images/GitHubKitty.png"/></a>
+							</div>
+
+								<div class="col-md-2" style={{"float": "right"}}></div>
+
+								<div class="col-md-5" style={{"float": "right"}}>
+									<a  href="https://bulbasaurguacshooter.herokuapp.com/" rel="noopener noreferrer" target="_blank"><img class="work" src="images/BulbasaurGuac.png"/></a>
+								</div>
+
+						</div>
+
+
+
+	
+								<br/><br/><br/><hr/><br/><br/><br/>
+
+
+						<div class="row">
+
+							<div class="col-md-7">
+			
+								<a rel="noopener noreferrer" target="_blank"><img class="work" src="images/pier.png"/></a>
+			
+							</div>
+
+
+
+							<div class="col-md-5">
+
+								<h4>LASA</h4>
+								<br/>
+									<p>I am working on a project that crowd sources personal healthcare practices to support mental health. The project will include a RESTful structure and a database that receives crowd sourced information. Research has been conducted with mental health professionals and lawyers to ensure the best outcome for users. We expect a launch by January 2018.
+
+									<br/><br/>
+									Built Using: React. Ruby. Sinatra. SQL.
+
+									</p>
+
+								<a href="https://github.com/gperez13" rel="noopener noreferrer" target="_blank"><img id="kittycat" src="images/GitHubKitty.png"/></a>
+							</div>
+
+						</div>				
+
+
+
 				</div>
+			</div> 
+		</div>
+		</div>
+
+		)
 
 
-			</div>
-
-			)
+			
 	}
 }
 
